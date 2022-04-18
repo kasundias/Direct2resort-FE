e366c484a82d220737d2f7eded5d8dadd36d113a
Kasun Dias <kmgknd@gmail.com>
2022-04-18 18:44:38 +0530

    ssdfsdf

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -20,16 +20,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'ivys-proj'`, () => {
+  it(`should have as title 'xd-proj'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.debugElement.componentInstance;
-    expect(app.title).toEqual('ivys-proj');
+    expect(app.title).toEqual('xd-proj');
   });
 
   it('should render title in a h1 tag', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.debugElement.nativeElement;
-    expect(compiled.querySelector('h1').textContent).toContain('Welcome to ivys-proj!');
+    expect(compiled.querySelector('h1').textContent).toContain('Welcome to xd-proj!');
   });
 });
